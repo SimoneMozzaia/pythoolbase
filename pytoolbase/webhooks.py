ac035913a804fd2787bf5b8a57d76f989c9b3d09
Simone Mozzaia <simone.mozzaia@moncler.com>
2024-07-22 15:20:58 +0200

    Added TeamsWebhook

--- pytoolbase/webhooks.py
+++ pytoolbase/webhooks.py
@@ -37,3 +37,38 @@ class GoogleWebhook:
         )
         
         self.__custom_logger.debug(f"response: {response}")
+
+
+class MicrosoftTeamsWebhook:
+    __custom_logger = None
+    __app_message = None
+    __hook_url = None
+
+    def __init__(self, hook_url):
+        self.__custom_logger = CustomLogger('MicrosoftTeamsWebhook').custom_logger(logging.INFO)
+        self.__custom_logger.info(f'Initializing MicrosoftTeamsWebhook Class')
+        self.__hook_url = hook_url
+
+    def send_message_to_teams_chat(self, *args):
+        self.__custom_logger.info('send_message_to_teams_chat')
+        self.__create_app_message_for_webhook(*args)
+        self.__trigger_teams_webhook()
+
+    def __create_app_message_for_webhook(self, *args):
+        self.__custom_logger.info('__create_app_message_for_webhook')
+        self.__app_message = {"output": f"{args[0]}"}
+
+    def __trigger_teams_webhook(self):
+        self.__custom_logger.info(f"__trigger_teams_webhook")
+
+        app_message = self.__app_message
+        message_headers = {"Content-Type": "application/json; charset=UTF-8"}
+        http_obj = Http()
+        response = http_obj.request(
+            uri=self.__hook_url,
+            method="POST",
+            headers=message_headers,
+            body=dumps(app_message),
+        )
+
+        self.__custom_logger.debug(f"response: {response}")
